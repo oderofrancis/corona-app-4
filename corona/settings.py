21f09f33518ed81c1fe2d935b0489173b3fbb222
oderofrancis <francisodero10@gmail.com>
2022-08-16 18:46:02 +0300

    corona updates v4

--- corona/settings.py
+++ corona/settings.py
@@ -12,11 +12,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# database deployment
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
